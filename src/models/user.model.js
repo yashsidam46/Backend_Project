be6a007599d500a8da4c8d787162c9394ac39d96
Yash Sidam <sidamyash46@gmail.com>
2026-07-21 23:12:59 +0530

    ACCESS TOKEN AND REFRESH TOKEN

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,5 +57,29 @@ userSchema.methods.isPasswordCorrect = async function (password)
 {
  return await bcrypt.compare(password,this.password)    
 }
+//jwt -> bearer token
+userSchema.methods.generateAccessToken = function(){
+ return jwt.sign(
+    {
+        _id : this._id,
+        email : this.email,
+        username : this.username,
+        fullname : this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECREATE,{
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
 
+userSchema.methods.generateRefreshToken = function () {
+return jwt.sign(
+    {
+        _id : this._id
+    },
+    process.env.REFRESH_TOKEN_SECREATE,{
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 export const user = mongoose.model("User",userSchema)
