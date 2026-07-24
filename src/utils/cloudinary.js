5b4dcbe3847e46d3dd3010b1b30e9ffb13a26332
Yash Sidam <sidamyash46@gmail.com>
2026-07-24 23:33:25 +0530

    claudinary upload file

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,19 @@ claodinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret : process.env.CLOUDINARY_API_SECREATE
 })
+
+const uploadAtCloudinary = async (localFilePath) =>{
+    try {
+        if(!localFilePath) return null
+       const responce = await claodinary.uploader.upload(localFilePath, {
+            resource_type : "auto"
+        })
+        console.log("file uploaded on cloudinary",responce.url);
+        return responce;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)//remove the locally saved 
+        //temp file as the upload operation got failed
+    }
+}
+export {uploadAtCloudinary}
