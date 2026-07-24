7719198bb437412ee8bcc0a5801d7ca05c1fed98
Yash Sidam <sidamyash46@gmail.com>
2026-07-25 00:10:51 +0530

    test api

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 import userRouter from './routes/user.routes.js'
 // routes declaration
 
-app.use("/api/v1/users" , userRouter)
+app.use("/api/v1/users", userRouter)
 
 
 export { app }
