0baf711133fa058c4625616466a9040e830a0080
Yash Sidam <sidamyash46@gmail.com>
2026-07-28 23:14:23 +0530

    algo for registering user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,3 +3,14 @@ export const registerUser = (req, res) => {
     message: "user registered !!!!!",
   });
 };
+
+//register a user 
+//get user details from frontend
+//username,gmail,name,
+//validation -> not empty
+//check if user already exist ir not
+//check for avatar image 
+//uplaod on claudnary,avatar
+//create user object - create entry in db
+//remove password and refresh token field from responce
+//retrun responce
